2eb1130f2aa9dbf7e042a522c616a6e208cca3cc
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-03-21 03:06:12 +1100

    refractored app.tsx

--- main_project/src/App.tsx
+++ main_project/src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Cards from "./components/Cards";
+import Stat from "./components/Stat.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello, Erman Mingzhi & Aneri!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Testing that Netlify is working :)
-      </p>
+        <div className="bg-myPurple">
+
+            <NavBar/>
+            <Hero/>
+            <Cards/>
+            <Stat/>
+        </div>
     </>
   )
 }
