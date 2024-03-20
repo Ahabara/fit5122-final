f7ff06d208a9856f6febd28dd3f65045d5c59cf6
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-03-21 03:05:01 +1100

    styled navbar

--- main_project/src/components/NavBar.tsx
+++ main_project/src/components/NavBar.tsx
@@ -2,21 +2,21 @@ import logo from "../assets/Logo.png"
 
 const NavBar = () => {
     return (
-            <nav className="bg-red-100 border-gray-200 dark:bg-gray-900 py-8 px-12">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pl-2 bg-[#fab82c]">
+            <nav className="bg-red-100  dark:bg-gray-900 py-8 px-12">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto border border-black pl-2 bg-[#fab82c]">
                     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src={logo} className="h-12" alt="ForReal Logo"/>
                         <span
                             className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ForReal</span>
                     </a>
-                    <div className="flex md:order-2 bg-[#b686fe] p-4">
+                    <div className="flex md:order-2 bg-[#b686fe] p-4 border-l border-black">
                         <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
                                 aria-expanded="false"
                                 className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                  viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                      strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
                             <span className="sr-only">Search</span>
                         </button>
@@ -24,13 +24,13 @@ const NavBar = () => {
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                          strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                 </svg>
                                 <span className="sr-only">Search icon</span>
                             </div>
                             <input type="text" id="search-navbar"
-                                   className="block w-full p-2 ps-10 text-sm text-gray-900 border bg-purple-400 border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                   className="block w-full p-2 ps-10  border border-black text-sm text-gray-900    bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                    placeholder="Search..."/>
                         </div>
                         <button data-collapse-toggle="navbar-search" type="button"
@@ -39,8 +39,8 @@ const NavBar = () => {
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                  viewBox="0 0 17 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                      strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                             </svg>
                         </button>
                     </div>
@@ -50,8 +50,8 @@ const NavBar = () => {
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                          strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                 </svg>
                             </div>
                             <input type="text" id="search-navbar"
