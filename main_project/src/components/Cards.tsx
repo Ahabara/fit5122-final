b7160d5c0b633917d2b650ca05cca4d37c177fed
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-03-23 13:00:00 +1100

    removed bg color

--- main_project/src/components/Cards.tsx
+++ main_project/src/components/Cards.tsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 const Cards = () => {
     return (
-        <div className={"bg-red-500 h-screen"}>
+        <div className={""}>
             <div
                 className={"container mx-auto flex space-between gap-12 overflow-x-auto md:overflow-visible -translate-y-32"}>
                 <Card/>
