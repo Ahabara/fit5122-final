5e904f109998fece8dcf28779ea51407403d33f8
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-04-03 00:26:44 +1100

    feat: installed react router for routing

--- main_project/src/main.tsx
+++ main_project/src/main.tsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Country from "./components/countries/Country.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/*",
+        element: <App/>,
+        errorElement: <App/>,
+    },
+
+    {
+        path: "/country/:countryName",
+        element: <Country/>,
+        errorElement: <App/>,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>,
 )
