37aca185d45b061094470f00f2ec9d85ded37d68
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-04-04 00:38:56 +1100

    refractor: removed unneeded imports

--- main_project/src/App.tsx
+++ main_project/src/App.tsx
@@ -1,25 +1,22 @@
-import './App.css'
+import "./App.css";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
-// import Stat from "./components/Stat.tsx";
 import ExploreCultures from "./components/landing/ExploreCultures.tsx";
-// import Quiz from "./components/landing/Quiz.tsx";
 import Footer from "./components/Footer.tsx";
-import {GamePage} from "./components/landing/GamePage.tsx";
+import { GamePage } from "./components/landing/GamePage.tsx";
 
 function App() {
   return (
     <>
-        <div className="bg-myPurple">
-
-            <NavBar/>
-            <Hero/>
-            <ExploreCultures/>
-            <GamePage/>
-            <Footer/>
-        </div>
+      <div className="bg-myPurple">
+        <NavBar />
+        <Hero />
+        <ExploreCultures />
+        <GamePage />
+        <Footer styling={"bg-purple-900 text-white"} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
