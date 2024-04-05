ba45b50f1c521993e7a4b0f96b8f2c395a004aa9
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-04-06 03:38:21 +1100

    fix: href added

--- main_project/src/components/explore/ExploreHero.tsx
+++ main_project/src/components/explore/ExploreHero.tsx
@@ -20,9 +20,11 @@ const ExploreHero = () => {
               essence of this dynamic city.
             </p>
             <div className="flex justify-center">
-              <button className="inline-flex rounded border-0 bg-indigo-500 px-6 py-2 text-lg text-white hover:bg-indigo-600 focus:outline-none">
-                Take Our Quiz
-              </button>
+              <a href="/explore#quiz">
+                <button className="inline-flex rounded border-0 bg-indigo-500 px-6 py-2 text-lg text-white hover:bg-indigo-600 focus:outline-none">
+                  Take Our Quiz
+                </button>
+              </a>
             </div>
           </div>
           <div className="w-5/6 md:w-1/2 lg:w-full lg:max-w-lg">
