f4f9be58fe16a80beb2d6d6a0389b0d05531c5f1
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-04-06 03:42:27 +1100

    feat: beautiful line chart

---
+++ main_project/src/components/charts/YearOfArrival.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+interface DataProps {
+  data: { name: string; value: number }[];
+}
+
+const YearOfArrival: React.FC<DataProps> = ({ data }) => {
+  return (
+    <div className="overflow-scroll md:overflow-visible">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      </LineChart>
+    </div>
+  );
+};
+
+export default YearOfArrival;
