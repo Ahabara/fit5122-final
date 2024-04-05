871a0763c7641f45e6a70da656c97f7cea01a2cb
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-04-06 03:39:51 +1100

    fix: added key

--- main_project/src/components/countries/MostSpokenLanguages.tsx
+++ main_project/src/components/countries/MostSpokenLanguages.tsx
@@ -1,25 +1,35 @@
 import React from "react";
 
 interface MostSpokenLanguagesProps {
-    languages: string[]
+  languages: string[];
 }
 
-export const MostSpokenLanguages: React.FC<MostSpokenLanguagesProps> = ({languages}) => {
-    return <>
-        <div className="bg-white shadow-md rounded-md overflow-hidden max-w-lg">
-            <div className="bg-gray-100 py-2 px-4">
-                <h2 className="text-xl font-semibold text-gray-800">Most Spoken Languages</h2>
-            </div>
-            {languages.map((language, i) => (
-                <ul className="divide-y divide-gray-200">
-                    <li className="flex items-center py-4 px-6">
-                        <span className="text-gray-700 text-lg font-medium mr-4">{i + 1}.</span>
-                        <div className="flex-1 mr-4">
-                            <h3 className="text-lg font-medium text-gray-800">{language}</h3>
-                        </div>
-                    </li>
-                </ul>
-            ))}
+export const MostSpokenLanguages: React.FC<MostSpokenLanguagesProps> = ({
+  languages,
+}) => {
+  return (
+    <>
+      <div className="max-w-lg overflow-hidden rounded-md bg-white shadow-md">
+        <div className="bg-gray-100 px-4 py-2">
+          <h2 className="text-xl font-semibold text-gray-800">
+            Most Spoken Languages
+          </h2>
         </div>
+        {languages.map((language, i) => (
+          <ul className="divide-y divide-gray-200" key={i}>
+            <li className="flex items-center px-6 py-4">
+              <span className="mr-4 text-lg font-medium text-gray-700">
+                {i + 1}.
+              </span>
+              <div className="mr-4 flex-1">
+                <h3 className="text-lg font-medium text-gray-800">
+                  {language}
+                </h3>
+              </div>
+            </li>
+          </ul>
+        ))}
+      </div>
     </>
-}
+  );
+};
