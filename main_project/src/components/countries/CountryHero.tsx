5a39bd6fcf5c9e3963b5c8290dbc4d696b7f7f3a
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-04-07 20:57:18 +1000

    fix: styling bug resolved

--- main_project/src/components/countries/CountryHero.tsx
+++ main_project/src/components/countries/CountryHero.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 
 interface CountryHeroProps {
-    countryName?: string,
-    about: string
+  countryName: string;
+  about: string;
 }
 
-export const CountryHero: React.FC<CountryHeroProps> = ({countryName, about}) => {
-    return (
-        <>
-            <h1 className="pt-8 lg:pt-0 font-extrabold text-5xl pb-24">
-                Learn More About
-                <strong className=" font-extrabold text-white"> <span
-                    className="bg-myPurple ">{countryName?.toUpperCase()}</span> </strong>
-            </h1>
-            <div className="flex flex-wrap w-full mb-20">
-                <div className="lg:w-1/5 align-middle w-full mb-6 lg:mb-0">
-                    <h1 className="sm:text-6xl text-2xl font-medium title-font mb-2 text-gray-900">{countryName?.toUpperCase()}</h1>
-                </div>
-                <p className="lg:w-4/5 w-full leading-relaxed text-gray-500">{about}</p>
-            </div>
-        </>
-    );
+export const CountryHero: React.FC<CountryHeroProps> = ({
+  countryName,
+  about,
+}) => {
+  return (
+    <>
+      <h1 className="pb-24 pt-8 text-5xl font-extrabold lg:pt-0">
+        Learn More About
+        <strong className=" font-extrabold text-white">
+          {" "}
+          <span className="bg-myPurple ">
+            {countryName?.toUpperCase()}
+          </span>{" "}
+        </strong>
+      </h1>
+      <div className="mb-20 flex w-full flex-wrap">
+        <div className="mb-6 flex w-full gap-6 align-middle lg:mb-0">
+          <h1 className="title-font mb-2 text-2xl font-medium text-gray-900 sm:text-6xl">
+            {countryName?.toUpperCase()}
+          </h1>
+          <p className="w-full leading-relaxed text-gray-500 ">{about}</p>
+        </div>
+      </div>
+    </>
+  );
 };
