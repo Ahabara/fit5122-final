af4472a1d4deb6420fb10e1e7836ebcd371d73f4
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-04-16 14:25:56 +1000

    feat: bank rates page

---
+++ main_project/src/components/Finance/BankPage.tsx
@@ -0,0 +1,12 @@
+import Layout from "../landing/Layout.tsx";
+import BankRates from "./BankRates.tsx";
+
+const BankPage = () => {
+  return (
+    <Layout>
+      <BankRates />
+    </Layout>
+  );
+};
+
+export default BankPage;
