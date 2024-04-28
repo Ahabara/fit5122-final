d4c1a2e7604f9e8f0e0edd3d83c47188f11922b9
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-04-28 22:56:24 +1000

    feat: option to style footer

--- main_project/src/components/landing/Layout.tsx
+++ main_project/src/components/landing/Layout.tsx
@@ -4,16 +4,17 @@ import React from "react";
 
 interface LayoutProps {
   children?: React.ReactNode;
+  styling?: string;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children, styling }) => {
   return (
     <>
       <NavBar />
       <div className="bg-gradient-to-r from-violet-200 to-pink-200">
         {children}
       </div>
-      <Footer />
+      <Footer styling={styling} />
     </>
   );
 };
