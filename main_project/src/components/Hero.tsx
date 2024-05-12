11ad6cc82f9548cd4afbcb8f6eda5984fafca519
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-05-13 03:56:08 +1000

    feat: linked each section to each page

--- main_project/src/components/Hero.tsx
+++ main_project/src/components/Hero.tsx
@@ -46,7 +46,7 @@ const Hero = () => {
           </p>
           <div className="mt-8 flex flex-wrap gap-4 text-center">
             <a
-              href="/#Countries"
+              href="/#landing"
               className="group relative px-6 py-3 font-bold text-black"
             >
               <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-myPurple transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
