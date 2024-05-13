8ae92ef3d34d6822cd66e7c50a329afe187a6f8e
Abdelsalam Habara <ahab0008@student.monash.edu>
2024-05-14 00:28:34 +1000

    fix: link now goes to top of page

--- main_project/src/App.tsx
+++ main_project/src/App.tsx
@@ -4,8 +4,15 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer.tsx";
 import WorkHardPlayHard from "./WorkHardPlayHard.tsx";
 import JourneyMap from "./JourneyMap.tsx";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <>
       <JourneyMap />
